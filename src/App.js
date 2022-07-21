f47f16ccb30f960da78c59dd547a05f5a9044f12
Aditi Joshi <38169015+adyjoshi@users.noreply.github.com>
2022-07-21 13:51:24 -0230

    Test edits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React from 'react';
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Skills from './components/pages/skills/Skills';
+import Experiences from './components/pages/experiences/Experiences';
+import Educations from './components/pages/educations/Educations';
+import Portfolios from './components/pages/portfolios/Portfolios';
 import './App.css';
-import "materialize-css/dis/css/materialize.min.css";
-import "materialize-css/dis/js/materialize.min.js";
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Resume Website</h1>
-    </div>
+    <Router>
+      <>
+      <Route exact path="/" component={Home} />
+      <Switch>
+        <Route path="/skills" component={Skills} />
+        <Route path="/experiences" component={Experiences} />
+        <Route path="/educations" component={Educations} />
+        <Route path="/portfolios" component={Portfolios} />
+      </Switch>
+      </>
+    </Router>
   );
 }
 
